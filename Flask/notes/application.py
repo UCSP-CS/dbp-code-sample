51cf45b64fd8bdd39ed03e5d6f40b4fbf7240db6
Roger Peralta Aranibar <aranibar.roger@gmail.com>
2021-10-01 19:54:39 -0500

    feat(notes): Use session for notes

--- Flask/notes/application.py
+++ Flask/notes/application.py
@@ -7,12 +7,12 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-notes = []
-
 @app.route("/", methods=["GET", "POST"])
 def index():
+    if not session.get("notes"):
+        session["notes"] = []
     if request.method == "POST":
         note = request.form.get("note")
-        notes.append(note)
+        session["notes"].append(note)
 
-    return render_template("index.html", notes=notes)
+    return render_template("index.html", notes=session["notes"])
